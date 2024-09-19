ee3f91e7ac9ac0243d71d8ca421f8fe7a204d44c
Petra <milicevicpetra7@gmail.com>
2024-09-19 19:32:08 +0200

    searchbar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,24 +1,42 @@
 import landingImage from "../assets/landing.png";
 import appDownloadImage from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
+  const handleReset = () => {
+    console.log("Reset clicked!");
+  };
+
   return (
     <div className="flex flex-col gap-12">
-      <div className="bg-white rounded-1g shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+      <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
         <h1 className="text-5xl font-bold tracking-tight text-orange-600">
-          Try our delivery today!
+          Tuck into a takeway today
         </h1>
         <span className="text-xl">Food is just a click away!</span>
+        <SearchBar
+          placeHolder="Search by City or Town"
+          onSubmit={handleSearchSubmit}
+          onReset={handleReset}
+        />
       </div>
       <div className="grid md:grid-cols-2 gap-5">
         <img src={landingImage} />
         <div className="flex flex-col items-center justify-center gap-4 text-center">
           <span className="font-bold text-3xl tracking-tighter">
-            Order takaway even faster with our app!
+            Order takeaway even faster!
           </span>
           <span>
-            Download Nom-Nom Eats app for faster ordering and personalized
-            recommendations.
+            Download the MernEats App for faster ordering and personalised
+            recommendations
           </span>
           <img src={appDownloadImage} />
         </div>
@@ -26,4 +44,5 @@ const HomePage = () => {
     </div>
   );
 };
+
 export default HomePage;
